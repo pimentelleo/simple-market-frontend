5551a9c871c6a82cfb2f1e58791f5c1903c58314
Tayanne <tayh.snovaes@gmail.com>
2021-06-04 13:00:01 -0300

    [ADD] [WIP] Products Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,42 @@
-import React from "react";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import Link from "@material-ui/core/Link";
-import ProTip from "./ProTip";
-import SignInSide from "./SignInSide";
-import SignUp from "./SignUp";
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './ProTip';
+import SignInSide from './SignInSide';
+import SignUp from './SignUp';
+import Products from './Products';
+import Navigation from './Navigation';
 
-import Navigation from "./Navigation";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
+	return (
+		<Typography variant='body2' color='textSecondary' align='center'>
+			{'Copyright © '}
+			<Link color='inherit' href='https://material-ui.com/'>
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
 }
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <Navigation />
-        <Switch>
-          <Route path="/" component={SignInSide} exact />
-          <Route path="/registrar" component={SignUp} />
-          <Route component={Error} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div>
+				<Navigation />
+				<Switch>
+					<Route path='/' component={SignInSide} exact />
+					<Route path='/registrar' component={SignUp} />
+					<Route path='/produtos' component={Products} />
+					<Route component={Error} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
 }
